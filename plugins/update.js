a5587325b233a678d577ad47fe776f86b6084035
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-01-05 19:24:24 +0530

    [ Jessi-md 4.8.1 ⏱️ ]

---
+++ plugins/update.js
@@ -0,0 +1,16 @@
+let { execSync } = require('child_process')
+
+let handler = async (m, { conn, text, isROwner }) => {
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git remote set-url origin https://github.com/whiteshadowofficial/Jessi-md.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
+  }
+}
+handler.help = ['update']
+handler.tags = ['host']
+handler.command = /^update$/i
+
+handler.owner = true
+
+module.exports = handler
