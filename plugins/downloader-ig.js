ce505354937accd4dcf6c5c160ccb60218f6668f
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-01-19 13:50:22 +0530

    Update downloader-ig.jsl

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -3,10 +3,20 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
     await m.reply(`Ｌｏａｄｉｎｇ．．．`);
-    let tes = await fetch(`https://inrl-web.onrender.com/api/insta?url=${args[0]}`);
-            const json = await tes.json();
-            const igdl = json.result;
-            await conn.sendFile(m.chat, igdl, 'error.mp4', '❤️ done here', m);
+    
+    try {
+        let response = await fetch(`https://vihangayt.me/download/instagram2?url=${args[0]}`);
+        let data = await response.json();
+        
+        if (data.status) {
+            await conn.sendMessage(m.chat, { video: { url: data.data[0].download_link } }, m);
+        } else {
+            throw new Error('Error in response data');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        m.reply('Failed to fetch Instagram data. Please try again later.');
+    }
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')
@@ -14,4 +24,4 @@ handler.tags = ['downloader']
 
 handler.command = /^(ig(dl)?|insta|instagram(dl)?)$/i
 
-export default handler
+export default handler;
