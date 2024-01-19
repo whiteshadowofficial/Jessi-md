003f8d8eb832043cbda3d635df081f808e8bd25a
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-01-19 17:24:01 +0530

    [ Jessi-md 4.8.2 ⏱️ ]

--- plugins/enable.js
+++ plugins/enable.js
@@ -1,104 +1,4 @@
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
-  let isEnable = /true|enable|(turn)?on|1/i.test(command)
-  let chat =global.DATABASE.data.chats[m.chat]
-  let user = global.DATABASE.data.users[m.sender]
-  let bot = global.DATABASE.data.settings[conn.user.jid] || {}
-  let type = (args[0] || '').toLowerCase()
-  let isAll = false, isUser = false
-  switch (type) {
-    case 'welcome':
-      if (!m.isGroup) {
-        if (!isOwner) {
-          global.dfail('group', m, conn)
-          throw false
-        }
-      } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
-        throw false
-      }
-      chat.welcome = isEnable
-      break
-    case 'delete':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.delete = isEnable
-      break
-    case 'nsfw':
-        if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }}
-      chat.nsfw = isEnable
-      break
-      case 'premnsfwchat':
-        if (m.isGroup) {
-        if (!isROwner) {
-          global.dfail('rowner', m, conn)
-          throw false
-        }}
-      chat.premnsfw = isEnable
-      break
-      case 'autoSticker':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.autoSticker = isEnable
-      break
-      case 'antispam':
-       if (m.isGroup) {
-         if (!(isAdmin || isOwner)) {
-           global.dfail('admin', m, conn)
-           throw false
-         }
-       }
-       chat.antiSpam = isEnable
-       break
-       case 'anticall':
-       isAll = true
-         if (!isOwner) {
-           global.dfail('rowner', m, conn)
-           throw false
-       }
-       chat.anticall = isEnable
-       break
-    case 'antidelete':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.delete = !isEnable
-      break
-    case 'public':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['self'] = !isEnable
-      break
-    case 'antilink':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.antiLink = isEnable
-      break
-    case 'restrict':
-      isAll = true
-      if (!isOwner) {
-        global.dfail('owner', m, conn)
         throw false
       }
       bot.restrict = isEnable
@@ -118,6 +18,31 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
         throw false
       }
       global.opts['autoread'] = isEnable
+      break
+      case 'pmblocker':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.pmblocker = isEnable
+break	  
+case 'autobio':
+  isAll = true
+  if (!isROwner) {
+  global.dfail('rowner', m, conn)
+  throw false
+  }
+  bot.autoBio = isEnable
+  break
+  case 'chatbot':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.chatbot = isEnable
       break
     case 'pconly':
     case 'privateonly':
@@ -147,12 +72,27 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       global.opts['swonly'] = isEnable
       break
     default:
-      if (!/[01]/.test(command)) return m.reply(`
-List option: welcome | delete | public | antilink | antidelete | antitoxic | autolevelup | detect | document | whitelistmycontacts | restrict | nyimak | autoread | pconly | gconly | swonly
-Contoh:
-${usedPrefix}enable welcome
-${usedPrefix}disable welcome
-`.trim())
+      if (!/[01]/.test(command)) return m.reply(`◈──『 *ADMIN*』───⳹
+➬ welcome
+➬ antilink
+➬ nsfw
+➬ autosticker
+➬ antispam
+➬ antitoxic
+╰──────────⳹ 
+◈──『 *USERS*』───⳹
+➬ autolevelup
+➬ chatbot 
+╰──────────⳹
+◈──『 *OWNER*』───⳹
+➬ onlydm
+➬ grouponly
+➬ autotype
+➬ autobio
+╰──────────⳹
+*📌 Example :*
+*${usedPrefix}on* welcome
+*${usedPrefix}off* welcome`.trim())
       throw false
   }
   m.reply(`
