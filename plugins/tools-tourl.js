60bd579a185c1373c6c69066c6e07cca9fd84c39
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-01-20 17:29:09 +0530

    [ Jessi-md 4.8.2 ⏱️ ]

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -7,7 +7,6 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || q.mediaType || ''
-  if (!mime) throw `reply picture|video|audio|sticker|document with command .tourl`
 	let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 	let media = await q.download()
 	if (isTele && media.length < 5242880) {
