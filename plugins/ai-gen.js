f9cdb878346c7e4c7fcc3678d6dd5bfdd7585fb0
KaweeX <88151250+Kaweeshachamodx@users.noreply.github.com>
2024-01-28 10:58:21 +0530

    [ Jessi-md 4.8.4 ⏱️ ]

---
+++ plugins/ai-gen.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*This command generates images from text prompts*\n\n*𝙴xample usage*\n*◉ ${usedPrefix + command} Beautiful anime girl*\n*◉ ${usedPrefix + command} Elon Musk in pink output*`;
+
+  try {
+    m.reply('*Generating images*');
+
+    const endpoint = `https://cute-tan-gorilla-yoke.cyclic.app/imagine?text=${encodeURIComponent(text)}`;
+    const response = await fetch(endpoint);
+
+    if (response.ok) {
+      const imageBuffer = await response.buffer();
+      await conn.sendFile(m.chat, imageBuffer, 'image.png', null, m);
+    } else {
+      throw '*Image generation failed*';
+    }
+  } catch {
+    throw '*❌ Error generating image.*';
+  }
+};
+
+handler.help = ['gen < text >'];
+handler.tags = ['ai'];
+handler.command = ['aiimg', 'gen', 'imagine'];
+export default handler;
