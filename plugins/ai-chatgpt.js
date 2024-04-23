1f9207f8baa0628b01d3885fc191db8d9f9048d7
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-04-23 20:23:32 +0530

    Update ai-chatgpt.js

--- plugins/ai-chatgpt.js
+++ plugins/ai-chatgpt.js
@@ -4,8 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { text,  usedPrefix,  command }) => {
     if (!text) throw `Hey is there anything I can help you with??`
     let order = { text: wait, mentions: [m.sender], contextInfo: { forwardingScore: 256, isForwarded: true }};
-let aii = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${apilol}
-&text=${text}&user=user-unique-en`)
+let aii = await fetch(`https://aemt.me/v2/gpt4?text=${text}&user=user-unique-en`)
   let oke = await aii.json()
   let { key } = await conn.sendMessage(m.chat, order, { quoted: m });
   await new Promise(resolve => setTimeout(resolve, 2000));
